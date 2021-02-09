4e7b2c702b51698728ebdb39feb784eb3837ff73
DannyPadron09 <danny.padron09@gmail.com>
2021-02-09 13:04:57 -0500

    constants defined and input

---
+++ amountofpay.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// constants needed 
+#define PAYRATE 12.00
+#define TAXRATE_300 .15
+#define TAXRATE_150 .20
+#define TAXRATE_REST .25
+#define OVERTIME 40
+
+
+int main ()
+{
+    //declare variables
+    int hours = 0;
+    double pay = 0.0;
+    double taxes = 0.0;
+    double netPay = 0.0;
+
+    printf("Please enter the number of hours worked this week: ");
+    //get the input
+    scanf("%d", &hours);
+
+    // calculate the pay
+
+
+}
